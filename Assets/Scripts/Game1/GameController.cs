bc9348d5ce9c339c505bcfe0619d530f23b73a94
Miguel Langarano <miguelangarano@gmail.com>
2020-02-06 22:08:50 -0500

    first commit

---
+++ Assets/Scripts/Game1/GameController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public List<GameObject> fishesObjects = new List<GameObject>();
+    public GameObject currentFishObjectPosition;
+    public int difficulty = 0;
+    GameObject currentFish;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        selectFishColor();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void setGameDifficulty(int difficulty){
+        this.difficulty = difficulty;
+    }
+
+    //Set MainCamera Position for Game1
+    void setCameraPosition(){
+
+    }
+
+    void selectFishColor(){
+        switch (this.difficulty){
+            case 0:{
+                int ran = Random.Range(0,2);
+                setFishObjectInView(this.fishesObjects[ran]);
+                startFishJumping();
+                break;
+            }
+            case 1:{
+                break;
+            }
+            case 2:{
+                break;
+            }
+        }
+    }
+
+    void setFishObjectInView(GameObject fish){
+        Debug.Log("ok");
+        this.currentFish = Instantiate(fish, this.currentFishObjectPosition.transform.position, this.currentFishObjectPosition.transform.rotation);
+    }
+
+    void startFishJumping(){
+        int ran = Random.Range(0,2);
+        
+    }
+}
